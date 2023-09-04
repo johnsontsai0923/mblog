d5db7d5ab982e0d248249d010352bc6b66bc810b
johnsontsai0923 <johnsontsai0923@gmail.com>
2023-09-04 20:51:42 +0800

    Prsctice to 2.2.3

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from mysite.models import Post
+
+#admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','pub_date')
+
+admin.site.register(Post, PostAdmin)
